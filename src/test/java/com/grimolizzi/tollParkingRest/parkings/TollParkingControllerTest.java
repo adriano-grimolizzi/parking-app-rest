f582cbc5e767e23f6ffdb03a070c6f77c2563cbf
Adriano <a.grimolizzi@gmail.com>
2020-05-04 08:22:11 +0200

    feat: add controller test

--- src/test/java/com/grimolizzi/tollParkingRest/parkings/TollParkingControllerTest.java
+++ src/test/java/com/grimolizzi/tollParkingRest/parkings/TollParkingControllerTest.java
@@ -2,13 +2,14 @@ package com.grimolizzi.tollParkingRest.parkings;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,34 +22,37 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(TollParkingController.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TollParkingControllerTest {
 
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @MockBean
-//    private TollParkingRepository repository;
-//
-//    private static final String URL_TEMPLATE = "/tollParkings";
-//
-//    @Test
-//    public void shouldGetAll() throws Exception {
-//
-//        given(this.repository.findAll()).willReturn(getMockedList());
-//
-//        this.mvc.perform(get(URL_TEMPLATE)
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", hasSize(1)))
-//                .andExpect(jsonPath("$[0].name", equalTo("name")));
-//    }
-//
-//
-//    private List<TollParking> getMockedList() {
-//        List<TollParking> list = new ArrayList<>();
-//        list.add(new TollParking("code", "name"));
-//        return list;
-//    }
+    private static final String URL_TEMPLATE = "/tollParkings";
+
+    @InjectMocks
+    private TollParkingController controller;
+
+    @Mock
+    private TollParkingRepository repository;
+
+    private MockMvc mvc;
+
+    @Test
+    public void shouldGetAll() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+        this.mvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        given(this.repository.findAll()).willReturn(getMockedList());
+
+        this.mvc.perform(get(URL_TEMPLATE)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", equalTo("name")));
+    }
+
+    private List<TollParking> getMockedList() {
+        List<TollParking> list = new ArrayList<>();
+        list.add(new TollParking("code", "name"));
+        return list;
+    }
 }
--- src/test/java/com/grimolizzi/tollParkingRest/spots/ParkingSpotControllerTest.java
+++ src/test/java/com/grimolizzi/tollParkingRest/spots/ParkingSpotControllerTest.java
@@ -9,12 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
